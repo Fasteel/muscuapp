6e42ae548956303d18ec24cca6d94f244a7d5667
Fasteel <46248445+Fasteel@users.noreply.github.com>
2021-10-30 14:52:38 +0200

    :sparkles: As a user, I want to see my workout's (#7)

---
+++ backend/project/project/middlewares.py
@@ -0,0 +1,12 @@
+import time
+import os
+
+class TimeDelayMiddleware(object):
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        time.sleep(float(os.getenv('REQUEST_DELAY')))
+        response = self.get_response(request)
+        return response
--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'project.middlewares.TimeDelayMiddleware'
 ]
 
 ROOT_URLCONF = 'project.urls'
