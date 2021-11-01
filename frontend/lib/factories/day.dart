d78c8fe58f32389b59734dc49e471df08879fea1
Fasteel <46248445+Fasteel@users.noreply.github.com>
2021-11-01 14:05:51 +0100

    :bug: Transform key to translated day (#10)

--- frontend/lib/factories/day.dart
+++ frontend/lib/factories/day.dart
@@ -1,3 +1,13 @@
+const translations = {
+  "MON": "Monday",
+  "TUE": "Tuesday",
+  "WED": "Wednesday",
+  "THU": "Thursday",
+  "FRI": "Friday",
+  "SAT": "Saturday",
+  "SUN": "Sunday"
+};
+
 class Day {
   int id;
   String key;
@@ -9,5 +19,9 @@ class Day {
       id: json["id"],
       key: json["key"],
     );
+  }
+
+  static getTranslation(String key) {
+    return translations[key];
   }
 }
