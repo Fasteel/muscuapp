fedcf049b05f2b4d260a9182b4f3ebd614c55576
Fasteel <46248445+Fasteel@users.noreply.github.com>
2021-12-13 01:29:30 +0100

    :sparkles: Send the form to the backend (#17)

--- frontend/lib/screens/exercice_screen.dart
+++ frontend/lib/screens/exercice_screen.dart
@@ -1,8 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:muscuapp/model/workout.dart';
+import '../global_state.dart' as global_state;
+import 'package:http/http.dart' as http;
 
 class ExerciceScreen extends StatefulWidget {
-  const ExerciceScreen({Key? key}) : super(key: key);
+  const ExerciceScreen({Key? key, required this.workout}) : super(key: key);
+
+  final Workout workout;
 
   @override
   _ExerciceScreenState createState() => _ExerciceScreenState();
@@ -14,7 +23,7 @@ class _ExerciceScreenState extends State<ExerciceScreen> {
   final pauseDurationController = TextEditingController();
   final setNumberController = TextEditingController();
   final repetitionNumberController = TextEditingController();
-  final setWeightController = TextEditingController();
+  final weightController = TextEditingController();
   final positionController = TextEditingController();
 
   @override
@@ -25,7 +34,36 @@ class _ExerciceScreenState extends State<ExerciceScreen> {
             IconButton(
               icon: const Icon(Icons.check),
               onPressed: () async {
-                // TODO Send the form to the backend
+                final response = await http.post(
+                    Uri.parse('http://127.0.0.1:8000/exercices/'),
+                    headers: {
+                      HttpHeaders.authorizationHeader: global_state.token,
+                      HttpHeaders.contentTypeHeader: 'application/json'
+                    },
+                    body: jsonEncode({
+                      "title": titleController.text,
+                      "pause_duration": pauseDurationController.text,
+                      "set_number": setNumberController.text,
+                      "repetition_number": repetitionNumberController.text,
+                      "weight": weightController.text,
+                      "workout": widget.workout.id,
+                      "position": positionController.text
+                    }));
+                if (response.statusCode != 201) {
+                  Fluttertoast.showToast(
+                      msg: 'Failed to create exercice',
+                      gravity: ToastGravity.TOP,
+                      backgroundColor: Colors.red,
+                      fontSize: 18.0);
+                } else {
+                  Fluttertoast.showToast(
+                      msg: 'Exercice created',
+                      gravity: ToastGravity.TOP,
+                      backgroundColor: Colors.green,
+                      fontSize: 18.0);
+                }
+
+                Navigator.pop(context);
               },
             )
           ],
@@ -97,7 +135,7 @@ class _ExerciceScreenState extends State<ExerciceScreen> {
                   ),
                   const SizedBox(height: 20),
                   TextFormField(
-                    controller: setWeightController,
+                    controller: weightController,
                     keyboardType: TextInputType.number,
                     validator: (value) {
                       if (value!.isEmpty) {
