a92605aeaf865fbe11fc3f54c78470906396f666
Carl MONNERA <fasteel.web@gmail.com>
2021-12-25 18:27:24 +0100

    :recycle: Create base endpoint for each service file

--- frontend/lib/infrastructure/services/workout.dart
+++ frontend/lib/infrastructure/services/workout.dart
@@ -3,11 +3,14 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:muscuapp/application/models/workout.dart';
 import 'package:muscuapp/infrastructure/http.dart';
+import 'package:muscuapp/infrastructure/constants.dart' as constants;
 
 class WorkoutService {
+  static String endpoint = 'workouts/';
+
   static Future<List<Workout>> fetchAll() async {
     final response = await get(
-      Uri.parse('http://127.0.0.1:8000/workouts/'),
+      Uri.parse(constants.baseUrl + endpoint),
       headers: getDefaultHeader(),
     );
     if (response.statusCode != 200) {
@@ -19,20 +22,20 @@ class WorkoutService {
 
   static Future<Workout> fetch(int id) async {
     final response = await get(
-      Uri.parse('http://127.0.0.1:8000/workouts/' + id.toString()),
+      Uri.parse(constants.baseUrl + 'workouts/' + id.toString()),
       headers: getDefaultHeader(),
     );
     return Workout.fromJson(json.decode(response.body));
   }
 
   static Future<Response> create(String title, List<int> daysPK) {
-    return post(Uri.parse('http://127.0.0.1:8000/workouts/'),
+    return post(Uri.parse(constants.baseUrl + 'workouts/'),
         headers: getDefaultHeader(),
         body: jsonEncode({"title": title, "state": "AC", "days": daysPK}));
   }
 
   static Future<Response> update(int id, String title, List<int> daysPK) {
-    return put(Uri.parse('http://127.0.0.1:8000/workouts/' + id.toString()),
+    return put(Uri.parse(constants.baseUrl + 'workouts/' + id.toString()),
         headers: getDefaultHeader(),
         body: jsonEncode({"title": title, "state": "AC", "days": daysPK}));
   }
